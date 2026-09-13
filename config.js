85d9ac6dc499f1c816f8fc2c6fcb89aa6c374e1b
tutugsvy <tutugsvy@gmail.com>
2026-09-13 16:24:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@
    ========================================================================== */
 window.WSEX_CONFIG = {
   TOKEN: {
-    address: "0xac0f8c8a0dfdcaa415382622f0e3da23585d1b77", // ← ISI SETELAH LAUNCH
+    address: "0x0000000000000000000000000000000000000000", // ← ISI SETELAH LAUNCH
     symbol: "WSEX",
     name: "WALLSTREET.EXE",
     decimals: 18,
@@ -62,7 +62,7 @@ window.WSEX_CONFIG = {
       { symbol: "NVDA", address: "0xd0601ce157db5bdc3162bbac2a2c8af5320d9eec", decimals: 18 },
       { symbol: "SPCX", address: "0x4a0E65A3EcceC6dBe60AE065F2e7bb85Fae35eEa", decimals: 18 },
       { symbol: "TSLA", address: "0x322F0929c4625eD5bAd873c95208D54E1c003b2d", decimals: 18 },
-      { symbol: "AMZN", address: "0x12f190a9F9d7D37a250758b26824B97CE941bF54", decimals: 6 },
+      { symbol: "AMZN", address: "0x12f190a9F9d7D37a250758b26824B97CE941bF54", decimals: 18 },
       { symbol: "SPY", address: "0x117cc2133c37B721F49dE2A7a74833232B3B4C0C", decimals: 18 },
       { symbol: "AAPL", address: "0xaF3D76f1834A1d425780943C99Ea8A608f8a93f9", decimals: 18 },
     ],
