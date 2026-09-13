8bc9f27dc1771bc1ecc9d267ceb7df174d12b269
tutugsvy <tutugsvy@gmail.com>
2026-09-13 19:50:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -44,6 +44,19 @@ window.WSEX_CONFIG = {
     proMinHold: 1_000_000,
   },
 
+  MARKET_DATA: {
+  refreshMs: 15_000,
+
+  assets: [
+    { symbol: "NVDA", address: "0xd0601ce157db5bdc3162bbac2a2c8af5320d9eec" },
+    { symbol: "TSLA", address: "0x322F0929c4625eD5bAd873c95208D54E1c003b2d" },
+    { symbol: "AMZN", address: "0x12f190a9F9d7D37a250758b26824B97CE941bF54" },
+    { symbol: "SPY",  address: "0x117cc2133c37B721F49dE2A7a74833232B3B4C0C" },
+    { symbol: "AAPL", address: "0xaF3D76f1834A1d425780943C99Ea8A608f8a93f9" },
+    { symbol: "SPCX", address: "0x4a0E65A3EcceC6dBe60AE065F2e7bb85Fae35eEa" },
+  ],
+},
+
   LINKS: {
     buy: "https://www.ponsfamily.com/launchpad/{address}",
     chart: "https://www.geckoterminal.com/robinhood/tokens/{address}",
